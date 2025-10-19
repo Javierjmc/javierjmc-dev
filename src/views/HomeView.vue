1536ea086a0c442b5d85044fc459f9249d019dc9
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2025-10-19 23:40:43 +0000

    service vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import PostService from '@/services/PostService'
+import { ref, onMounted } from 'vue'
+
+const service = new PostService()
+const posts = service.getPosts()
+
+onMounted(async () => {
+  await service.fetchAll()
+})
+
+console.log(posts)
+</script>
+
+<template>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.title }}
+    </li>
+  </ul>
+</template>
